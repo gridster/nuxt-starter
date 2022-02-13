2aa28d9e1d7ac024e720cddc955ea78d2f333b71
teriell_ <42099803+gridster@users.noreply.github.com>
2022-02-13 20:26:16 +0100

    Initialize auth-supabase

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center">
+      <StarterKit/>
+      <p class="text-center">Find Supabase integrated as a plugin, and full-stack authentication support alongwith components like alert/toast, button, separator, modal/dialog, spinner, etc. </p>
+      <Sep/>
+      <div class="text-center">
+        <NuxtLink class="btn btn-primary btn-outline" to="/gallery">Browse Components</NuxtLink>&nbsp;
+        <NuxtLink class="btn btn-primary" to="/auth">See Supabase in action &rarr;</NuxtLink>
+      </div>
+  </div>
+</template>
